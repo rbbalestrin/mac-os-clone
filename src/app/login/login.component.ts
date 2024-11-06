9f297499d083b3b1e0507ebcc128616cd83600e4
rb.balestrin <therealrenanbez@gmail.com>
2024-11-06 16:22:12 -0300

    adiciona tela de login com estilos e controles de máquina

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { MachineControlsComponent } from '../machine-controls/machine-controls.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, MachineControlsComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
