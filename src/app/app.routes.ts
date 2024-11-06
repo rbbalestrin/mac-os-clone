f49d2d4694f1dd2892c65bb027ebc97e0f051edd
rb.balestrin <therealrenanbez@gmail.com>
2024-11-06 15:06:10 -0300

    adiciona componente de login e ajusta estilos globais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { BootComponent } from './boot/boot.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
     path: 'boot',
     component: BootComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '',
--- src/app/boot/boot.component.ts
+++ src/app/boot/boot.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-boot',
   standalone: true,
-  imports: [],
+  imports: [ProgressBarModule],
   templateUrl: './boot.component.html',
-  styleUrl: './boot.component.scss'
+  styleUrl: './boot.component.scss',
 })
 export class BootComponent {
+  progressBarValue: number = 0;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    let interval = setInterval(() => {
+      this.progressBarValue =
+        this.progressBarValue + Math.floor(Math.random() + 30);
+      if (this.progressBarValue >= 100) {
+        this.progressBarValue = 100;
+        clearInterval(interval);
+        this.router.navigate(['/login']);
+      }
+    }, 1500);
+  }
 }
