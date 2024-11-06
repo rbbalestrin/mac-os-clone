dc304a588a963896a8b24533fa28afbe609aa938
rb.balestrin <therealrenanbez@gmail.com>
2024-11-06 14:24:28 -0300

    adiciona tela de inicialização com estilos e imagens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { BootComponent } from './boot/boot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'boot',
+    component: BootComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/boot',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/boot',
+  },
+];
---
+++ src/app/boot/boot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boot',
+  standalone: true,
+  imports: [],
+  templateUrl: './boot.component.html',
+  styleUrl: './boot.component.scss'
+})
+export class BootComponent {
+
+}
